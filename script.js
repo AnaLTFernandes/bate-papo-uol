c7514b83c5c1ba5f80dba4ed8db1c93f5a35a7fc
AnaLTFernandes <analuizateixeirafernandes184@gmal.com>
2022-06-23 20:08:13 -0300

    Busca e renderização de mensagens incluida

--- script.js
+++ script.js
@@ -1,4 +1,59 @@
 
+let mensagens = [];
+
+buscarMensagens();
+
+function buscarMensagens() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(armazenarMensagens);
+}
+
+
+function armazenarMensagens(dados) {
+    mensagens = dados.data;
+    renderizarMensagens();
+}
+
+
+function renderizarMensagens() {
+    let ul = document.querySelector(".chat ul");
+    let mensagem;
+    let tipoMensagem;
+    let type;
+
+    for (let i = 0; i < mensagens.length; i++) {
+
+        mensagem = mensagens[i];
+        type = mensagem.type;
+
+        if (type === "status") {
+            tipoMensagem = 'mensagem-status';
+
+            ul.innerHTML += `
+            <li class = "${tipoMensagem}">
+                <p>(${mensagem.time})</p>
+                <h1><b>${mensagem.from}</b> ${mensagem.text}</h1>
+            </li>`
+        }
+
+        else {
+
+            if (type === "message") {
+                tipoMensagem = 'mensagem-publica';
+            }
+            if (type === "private_message") {
+                tipoMensagem = 'mensagem-particular';
+            }
+
+            ul.innerHTML += `
+            <li class = "${tipoMensagem}">
+                <p>(${mensagem.time})</p>
+                <h1><b>${mensagem.from}</b> para <b>${mensagem.to}</b>: ${mensagem.text}</h1>
+            </li>`
+        }
+    }
+}
+
 
 function toggleBarraLateral() {
     let background = document.querySelector(".background-aba-lateral");
@@ -11,7 +66,6 @@ function toggleBarraLateral() {
 
 function configurarMensagem(li) {
     let jaEscolhido = li.parentNode.querySelector(".escolhido");
-    
     
     if (jaEscolhido !== li) {
         jaEscolhido.classList.remove("escolhido");
