814372bc6b2c697abaa87407228715adba819358
Ana Fernandes <analuizateixeirafernandes184@gmail.com>
2022-12-06 15:35:19 -0300

    Layout responsivo e refatoração da tela inicial

--- script.js
+++ script.js
@@ -1,103 +1,90 @@
-
-let atualizarMensagens;
+const BASE_URL = "https://mock-api.driven.com.br/api/v6/uol";
+let destinatario = "Todos";
+let tipoMensagem = "message";
+let mensagens = [];
+const usuario = {};
 let mensagemEnviar;
 let usuariosAtivos;
-let usuario;
-let destinatario = 'Todos';
-let tipoMensagem = 'message';
-let mensagens = [];
 
+function entrarSala() {
+	document.querySelector(".telaDeEntrada .entrar").classList.toggle("hidden");
+	document.querySelector(".telaDeEntrada .entrando").classList.toggle("hidden");
 
-function entrandoNaSala() {
-    
-    document.querySelector(".telaDeEntrada .entrar").classList.toggle("hidden");
-    document.querySelector(".telaDeEntrada .entrando").classList.toggle("hidden");
+	usuario.name = document.querySelector(".telaDeEntrada input").value;
 
-    entrarSala();
+	enviarPedido();
+	enviarMensagemEnter();
 }
-
-
-function entrarSala () {
-
-    const nome = document.querySelector(".telaDeEntrada input").value;
-
-    usuario = {
-        name: nome
-    }
-
-    enviarPedido();
-    enviarMensagemEnter();
-}
-
 
 function enviarPedido() {
-    const pedido = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", usuario);
-    pedido.catch(tratarErro);
-    pedido.then(buscarMensagens);
+	axios
+		.post(`${BASE_URL}/participants`, usuario)
+		.catch(tratarErro)
+		.then(manterConexao);
 }
-
 
 function tratarErro(erro) {
-    if (erro.response.status === 400) {
-        alert("Já há um usuário online com esse nome. Por favor, insira outro.");
-        recarregarPagina();
-        return;
-    }
-    alert("Ocorreu um erro. Por favor, tente novamente.");
-    recarregarPagina();
+	if (erro.response.status === 400) {
+		alert("Já existe um usuário online com esse nome. Por favor, insira outro.");
+		return recarregarPagina();
+	}
+	alert("Ocorreu um erro. Por favor, tente novamente.");
+	recarregarPagina();
 }
 
+function manterConexao() {
+	setInterval(() => {
+		axios.post(`${BASE_URL}/status`, usuario);
+	}, 5000);
+
+	atualizarMensagens();
+}
+
+function atualizarMensagens() {
+	setInterval(buscarMensagens, 3000);
+}
 
 function buscarMensagens() {
-    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+	const promise = axios.get(`${BASE_URL}/messages`);
 
-    setInterval(() => {axios.post("https://mock-api.driven.com.br/api/v6/uol/status", ({name: usuario.name}))}, 5000);
-    promise.catch(tratarErro);
-    promise.then(armazenarMensagens);
+	promise.catch(tratarErro);
+	promise.then(armazenarMensagens);
 }
-
 
 function armazenarMensagens(dados) {
-    mensagens = dados.data;
-    usuariosAtivos = dados.data;
+	mensagens = dados.data;
+	usuariosAtivos = dados.data;
 
-    removerTelaInicial();
-    renderizarMensagens();
-    renderizarUsuarios();
+	removerTelaInicial();
+	renderizarMensagens();
+	renderizarUsuarios();
 }
-
 
 function removerTelaInicial() {
-    document.querySelector(".telaDeEntrada").classList.add("hidden");
+	document.querySelector(".telaDeEntrada").classList.add("hidden");
 }
-
 
 function renderizarMensagens() {
-    let ul = document.querySelector(".chat ul");
-    let mensagem;
-    let type;
-    ul.innerHTML = '';
+	let ul = document.querySelector(".chat ul");
+	let mensagem;
+	let type;
+	ul.innerHTML = "";
 
-    for (let i = 0; i < mensagens.length; i++) {
+	for (let i = 0; i < mensagens.length; i++) {
+		mensagem = mensagens[i];
+		type = mensagem.type;
 
-        mensagem = mensagens[i];
-        type = mensagem.type;
+		ul.innerHTML += mensagemTemplate(mensagem, type, i);
+	}
 
-        ul.innerHTML += mensagemTemplate(mensagem, type, i);
-    }
-
-    document.querySelector(".chat .ultimaMsg").scrollIntoView();
-
-    atualizarMensagens = setInterval(buscarMensagens, 3000);
+	document.querySelector(".chat .ultimaMsg").scrollIntoView();
 }
 
-
 function renderizarUsuarios() {
+	let ul = document.querySelector(".aba-lateral .usuarios-ativos");
+	let participantes = [];
 
-    let ul = document.querySelector(".aba-lateral .usuarios-ativos");
-    let participantes = [];
-
-    ul.innerHTML = `
+	ul.innerHTML = `
         <li onclick="configurarMensagem(this)">
             <div>
                 <ion-icon name="people"></ion-icon>
@@ -106,14 +93,17 @@ function renderizarUsuarios() {
             <ion-icon class="escolhido check" name="checkmark-outline"></ion-icon>
         </li>
     `;
-    
-    for (let i = 0; i < usuariosAtivos.length; i++) {
-        let participante = usuariosAtivos[i];
 
-        if (participante.type !== 'status' && !participantes.includes(participante.from)) {
-            participantes.push(participante);
+	for (let i = 0; i < usuariosAtivos.length; i++) {
+		let participante = usuariosAtivos[i];
 
-            ul.innerHTML += `
+		if (
+			participante.type !== "status" &&
+			!participantes.includes(participante.from)
+		) {
+			participantes.push(participante);
+
+			ul.innerHTML += `
                 <li onclick="configurarMensagem(this)">
                 <div>
                     <ion-icon name="person-circle"></ion-icon>
@@ -122,136 +112,136 @@ function renderizarUsuarios() {
                 <ion-icon class="check" name="checkmark-outline"></ion-icon>
                 </li>
             `;
-        }        
-    }
-
+		}
+	}
 }
 
-
 function mensagemTemplate(mensagem, type, index) {
-    if (index === 99) {
-        if (type === "status") {
-            return `
+	if (index === 99) {
+		if (type === "status") {
+			return `
             <li class = "mensagem-status ultimaMsg">
                 <p>(${mensagem.time})</p>
                 <h1><b>${mensagem.from}</b> ${mensagem.text}</h1>
-            </li>`
-        }
-    
-        if (type === "message") {
-            return `
+            </li>`;
+		}
+
+		if (type === "message") {
+			return `
             <li class = "mensagem-publica ultimaMsg">
                 <p>(${mensagem.time})</p>
                 <h1><b>${mensagem.from}</b> para <b>${mensagem.to}</b>: ${mensagem.text}</h1>
-            </li>`
-        }
-    
-        if (type === "private_message") {
-            return `
+            </li>`;
+		}
+
+		if (type === "private_message") {
+			return `
             <li class = "mensagem-particular ultimaMsg">
                 <p>(${mensagem.time})</p>
                 <h1><b>${mensagem.from}</b> reservadamente para <b>${mensagem.to}</b>: ${mensagem.text}</h1>
-            </li>`
-        }
-    }
+            </li>`;
+		}
+	}
 
-    if (type === "status") {
-        return `
+	if (type === "status") {
+		return `
         <li class = "mensagem-status">
             <p>(${mensagem.time})</p>
             <h1><b>${mensagem.from}</b> ${mensagem.text}</h1>
-        </li>`
-    }
+        </li>`;
+	}
 
-    if (type === "message") {
-        return `
+	if (type === "message") {
+		return `
         <li class = "mensagem-publica">
             <p>(${mensagem.time})</p>
             <h1><b>${mensagem.from}</b> para <b>${mensagem.to}</b>: ${mensagem.text}</h1>
-        </li>`
-    }
+        </li>`;
+	}
 
-    if (type === "private_message") {
-        return `
+	if (type === "private_message") {
+		return `
         <li class = "mensagem-particular">
             <p>(${mensagem.time})</p>
             <h1><b>${mensagem.from}</b> reservadamente para <b>${mensagem.to}</b>: ${mensagem.text}</h1>
-        </li>`
-    }
+        </li>`;
+	}
 }
-
 
 function enviarMensagemEnter() {
-    document.addEventListener("keydown", function(tecla) {
+	document.addEventListener("keydown", function (tecla) {
+		let teclaEnter = tecla.key === "Enter";
+		let mensagemValida =
+			document.querySelector(".campo-inferior textarea").value !== "";
 
-        let teclaEnter = (tecla.key === 'Enter');
-        let mensagemValida = (document.querySelector(".campo-inferior textarea").value !== '');
-
-        if (teclaEnter && mensagemValida) {
-            enviarMensagem();
-        }
-    })
+		if (teclaEnter && mensagemValida) {
+			enviarMensagem();
+		}
+	});
 }
-
 
 function enviarMensagem() {
+	msgDigitada = document.querySelector(".campo-inferior textarea").value;
 
-    msgDigitada = document.querySelector(".campo-inferior textarea").value;
+	mensagemEnviar = {
+		from: usuario.name,
+		to: destinatario,
+		text: msgDigitada,
+		type: tipoMensagem,
+	};
 
-    mensagemEnviar = {
-        from: usuario.name,
-        to: destinatario,
-        text: msgDigitada,
-        type: tipoMensagem
-    };
-    
-    let solicitacao = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagemEnviar);
-    solicitacao.catch(recarregarPagina);
-    solicitacao.then(buscarMensagens);
+	let solicitacao = axios.post(
+		"https://mock-api.driven.com.br/api/v6/uol/messages",
+		mensagemEnviar
+	);
+	solicitacao.catch(recarregarPagina);
+	solicitacao.then(buscarMensagens);
 
-    document.querySelector(".campo-inferior textarea").value = '';
+	document.querySelector(".campo-inferior textarea").value = "";
 }
-
 
 function recarregarPagina() {
-    window.location.reload();
+	window.location.reload();
 }
-
 
 function toggleBarraLateral() {
-    let background = document.querySelector(".background-aba-lateral");
-    background.classList.toggle("hidden");
+	let background = document.querySelector(".background-aba-lateral");
+	background.classList.toggle("hidden");
 
-    let barraLateral = document.querySelector(".aba-lateral");
-    barraLateral.classList.toggle("aba-lateral-aberta");
+	let barraLateral = document.querySelector(".aba-lateral");
+	barraLateral.classList.toggle("aba-lateral-aberta");
 }
-
 
 function configurarMensagem(li) {
-    let jaEscolhido = li.parentNode.querySelector(".escolhido");
-    
-    if (jaEscolhido !== li) {
-        jaEscolhido.classList.remove("escolhido");
-        li.querySelector(".check").classList.add("escolhido");
-    }
+	let jaEscolhido = li.parentNode.querySelector(".escolhido");
 
-    atualizarEstado(li);
+	if (jaEscolhido !== li) {
+		jaEscolhido.classList.remove("escolhido");
+		li.querySelector(".check").classList.add("escolhido");
+	}
+
+	atualizarEstado(li);
 }
 
-
 function atualizarEstado(li) {
-    let secaoUsuarios = document.querySelector(".usuarios-ativos .escolhido").parentElement;
-    let secaoVisibilidade = document.querySelector(".visibilidade .escolhido").parentElement;
+	let secaoUsuarios = document.querySelector(
+		".usuarios-ativos .escolhido"
+	).parentElement;
+	let secaoVisibilidade = document.querySelector(
+		".visibilidade .escolhido"
+	).parentElement;
 
-    destinatario = secaoUsuarios.querySelector("h3").innerHTML;
-    
-    tipoMensagem = secaoVisibilidade.querySelector("h3").innerHTML.toLowerCase();
+	destinatario = secaoUsuarios.querySelector("h3").innerHTML;
 
-    document.querySelector(".campo-inferior div p").innerHTML = `Enviando para ${destinatario} (${tipoMensagem})`;
+	tipoMensagem = secaoVisibilidade.querySelector("h3").innerHTML.toLowerCase();
 
-    if (tipoMensagem === "público") {
-        tipoMensagem = "message";
-        return;
-    }
-    tipoMensagem = 'private_message';
+	document.querySelector(
+		".campo-inferior div p"
+	).innerHTML = `Enviando para ${destinatario} (${tipoMensagem})`;
+
+	if (tipoMensagem === "público") {
+		tipoMensagem = "message";
+		return;
+	}
+	tipoMensagem = "private_message";
 }
